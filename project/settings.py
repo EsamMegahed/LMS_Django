d5c0ea2a8e607eb34501f57e43302e3166c2042e
EsamMegahed <esammegahed34366@gmail.com>
2024-05-28 23:36:22 +0300

    fix static files error

--- project/settings.py
+++ project/settings.py
@@ -119,16 +119,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
+    os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = BASE_DIR / 'static_files'
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = [ BASE_DIR / 'media' ]
+# MEDIA URL
+MEDIA_URL = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+
+
+
+# STATICFILES_DIRS = [BASE_DIR / "static",]
+# STATIC_ROOT = os.path.join(BASE_DIR, "statics/")
+# # STATIC_ROOT = BASE_DIR / 'static_files'
+# # Media files
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = [ BASE_DIR / 'media' ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
