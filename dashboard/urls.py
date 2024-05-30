c2fc582e9d3bf504efa6812a40be65b9403e3a9e
EsamMegahed <esammegahed34366@gmail.com>
2024-05-30 08:04:41 +0300

    add to dashboard user managment(view,delete,update)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
 from . import views
-
+appname = 'dashboard'
 urlpatterns = [
-    path('', views.dash),
+    path('', views.dash,name='users_view'),
+    path('details/<int:id>', views.user_detail,name='details'),
+    path('aaaa/<int:id>', views.user_update,name='update'), 
+
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .models import UserView
+from django.contrib.auth.models import User
+from .forms import UserForms
 # Create your views here.
 
 def dash(request):
-    context= {}
-    return render(request, "dashboard_base.html", context)
+    users = User.objects.all()
+    form = UserForms()
+    context = {'users':users}
+    return render(request, "user_view.html", context)
+
+def user_detail(request,id):
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        user.delete()
+        return redirect(reverse("users_view"))
+    context= {
+        'user':user,
+        
+    }
+    return render(request, "user_details.html", context)
+
+
+def user_update(request,id):
+    user = User.objects.get(id= id)
+    
+    if request.method == 'POST':
+        form = UserForms(request.POST,instance=user)
+        if form.is_valid():
+            form.save() 
+            return redirect(reverse("details",args=[user.id]))
+    else:
+        form = UserForms(instance=user)
+    context = {
+        'form' : form
+    }
+    return render(request, "user_update.html", context)
